cedf18577140435f6d5c4bdecd7264da4c6e3c8a
Mayara Oliveira <mayoliva0931@gmail.com>
2022-07-14 19:31:12 -0300

    Projeto Java

---
+++ src/br/com/generation/Backend/Backend.java
@@ -0,0 +1,51 @@
+package br.com.generation.Backend;
+
+public class Backend {
+	private String Text;
+	
+	public void Backtext() {
+		
+		System.out.println("----- O que é Backend -----");
+		System.out.println();
+		
+		System.out.println("Como o próprio nome sugere, vem da ideia daquilo que tem por trás de uma aplicação.");
+		System.out.println("O Back-End trabalha em boa parte dos casos fazendo a ponte");
+		System.out.println("entre os dados que vem do navegador rumo ao banco de dados e vice-versa.\n");
+		
+	}
+	
+	public void Logica() {
+		
+		System.out.println(" -- Logica de programação -- ");
+		System.out.println();
+		
+		System.out.println("É comum para quem está começando como programador ouvir falar sobre isso, porém poucos.");
+		System.out.println("dão a devida importância.");	
+		System.out.println("Aguçar seu entendimento e raciocínio lógico faz com que você resolva questões não apenas");
+		System.out.println("matemáticas, mas assuntos do cotidiano com mais facilidade.\n");
+				
+		System.out.println();
+		
+		ListTech();
+	}
+	
+	public void ListTech() {
+		
+		System.out.println(" -- Tecnologias para começar -- ");
+		System.out.println();
+		
+		System.out.println("* Java");
+		System.out.println("* PHP");
+		System.out.println("* Python para Web");
+		System.out.println("* ASP.NET Core REST APIs");
+		System.out.println("* Node.js com Express");
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ src/br/com/generation/BancoDados/BancoDados.java
@@ -0,0 +1,50 @@
+package br.com.generation.BancoDados;
+
+public class BancoDados {
+
+	public void BancoText() {
+		
+		System.out.println("---- O que é Banco de Dados ? ----");
+		System.out.println();
+		
+		System.out.println("Banco de dados é um conjunto de informações organizadas, geralmente estruturadas"); 
+		System.out.println("em linhas e colunas, em uma série de tabelas.\n");
+		
+		ListBanco();
+		
+	}
+	
+	public void ListBanco() {
+		
+		System.out.println(" --- O que estudar ? ---");
+		System.out.println();
+		
+		System.out.println("* Matemática básica");
+		System.out.println("* Lógicas de programação");
+		System.out.println("* Arquitetura de computadores");
+		System.out.println("* Estatística");
+		System.out.println("* Cultura organizacional");
+		System.out.println("* Modelagem de dados relacional");
+		System.out.println("* Gerência de projetos");
+		System.out.println("* Governança de Tecnologia da Informação");
+		System.out.println("* Big Data");
+		System.out.println("* Business Intelligence, entre outras\n");
+		
+		TipoBancos();
+	}
+	
+	public void TipoBancos() {
+		
+		System.out.println("--- Principais Banco de dados ---");
+		System.out.println();
+		
+		System.out.println("* SQL Server");
+		System.out.println("* MySQL");
+		System.out.println("* PostgreSQL");
+		System.out.println("* MongoDB");
+		System.out.println("* NoSQL");
+		
+		
+	}
+	
+}
